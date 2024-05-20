b10ffcd53911cb549433de930f8926e6c84e5d42
Deepak-tect <dddeepak888@gmail.com>
2024-05-21 03:15:04 +0530

    comment

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 const verifyJWT = async(req,res,next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        console.log(req.header("Authorization"))
+        // console.log(req.header("Authorization"))
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
